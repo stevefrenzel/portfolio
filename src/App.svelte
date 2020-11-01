31bd9f3ab12857b5883821cf0482ae97fe89e6ed
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-01 08:32:25 +0100

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Navigation from "./components/Navigation.svelte";
+  import Welcome from "./components/Welcome.svelte";
+  import About from "./components/About.svelte";
+  import Services from "./components/Services.svelte";
+  import Contact from "./components/Contact.svelte";
+  import Footer from "./components/Footer.svelte";
+</script>
+
+<style>
+  /* main {
+    max-height: 100vh;
+    overflow-y: scroll;
+    scroll-snap-type: y proximity;
+  } */
+</style>
+
+<a href="#main">Skip to main content</a>
+
+<!-- <Navigation /> -->
+<!-- <Welcome />
+<About /> -->
+<Services />
+<!-- <Contact /> -->
+<!-- <Footer /> -->
