82a8918dfe26f5daf279e48f92fdf56d235d6192
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-01 08:32:31 +0100

    Create Welcome.svelte

---
+++ src/components/Welcome.svelte
@@ -0,0 +1,16 @@
+<style>
+  /* section {
+    width: 100vw;
+    height: 100vh;
+    background-color: lightblue;
+  } */
+</style>
+
+<section id="welcome">
+  <h1>Steve Frenzel - Web Developer</h1>
+  <p>
+    I build things for the web, focused on
+    <mark>frontend</mark>
+    work.
+  </p>
+</section>
