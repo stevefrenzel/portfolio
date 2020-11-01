1a3a23b3de0f55f2112934cf52c5cdc21f660f42
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-01 08:32:35 +0100

    Create Navigation.svelte

---
+++ src/components/Navigation.svelte
@@ -0,0 +1,22 @@
+<style>
+
+</style>
+
+<header>
+  <nav>
+    <ul>
+      <li>
+        <a href="#welcome">Steve Frenzel - Web Developer</a>
+      </li>
+      <li>
+        <a href="#about">About</a>
+      </li>
+      <li>
+        <a href="#services">Services</a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
