9175ba854e0519977482af60a0ba743ac946e282
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-01 08:32:27 +0100

    Create About.svelte

---
+++ src/components/About.svelte
@@ -0,0 +1,36 @@
+<style>
+  /* section {
+    width: 100vw;
+    height: 100vh;
+    background-color: lightcoral;
+  } */
+</style>
+
+<section id="about">
+  <h2>About me</h2>
+  <p>
+    I’ve worked for a number of exciting startups and established companies. My
+    main goal is to
+    <mark>translate your vision</mark>
+    to your customers or visitors. In order to achieve this, I constantly
+    educate myself to be able to decide
+    <mark>which technology will best fit</mark>
+    your project.
+  </p>
+  <p>
+    First of all I would like to
+    <mark>get to know you</mark>
+    in order to give your idea a form. Based on this, I will develop an
+    <mark>interactive design of the website (or app)</mark>
+    together with you, which will serve as the foundation for the final product.
+  </p>
+  <p>
+    The result will be an
+    <mark>intuitive</mark>
+    ,
+    <mark>performant</mark>
+    and easily
+    <mark>accessible</mark>
+    product, taking current development best practices into account.
+  </p>
+</section>
