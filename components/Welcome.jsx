729505baa17df6a161a515c5a38d08bbe2b81379
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-07 10:13:50 +0100

    Delete Welcome.jsx

--- components/Welcome.jsx
+++
@@ -1,8 +0,0 @@
-export default function Welcome() {
-  return (
-    <section id='welcome'>
-      <h1>Steve Frenzel - Web Developer</h1>
-      <p>I build things for the web.</p>
-    </section>
-  );
-}
