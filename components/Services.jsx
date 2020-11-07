b532b10100e5c537265e6e18d277ce05f3cb2b12
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-07 09:35:18 +0100

    Create Services.jsx

---
+++ components/Services.jsx
@@ -0,0 +1,28 @@
+export default function Services() {
+  return (
+    <section id='services'>
+      <h2>Services</h2>
+      <h3>1. First meeting and planning</h3>
+      <p>
+        Meeting in person or via video call to get to know each other and
+        discuss the scope and cost of the project. (Free of charge!)
+      </p>
+      <h3>2. Creating a draft</h3>
+      <p>
+        Developing an interactive design of the project. Either as design in
+        Sketch or live (and privat) on Netlify.
+      </p>
+      <h3>3. Building the project</h3>
+      <p>
+        Developing your website (or app) with Accessibility, Best Practices,
+        Performance and SEO in mind. To measure these values, I use Google
+        Lighthouse, among others.
+      </p>
+      <h3>4. Maintenance</h3>
+      <p>
+        If desired, I can maintain the project for a monthly fee. Prices depend
+        on the size of it and duration of support.
+      </p>
+    </section>
+  );
+}
