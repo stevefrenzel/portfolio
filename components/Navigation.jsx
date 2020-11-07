911aa4d2596acda1528d2c2ea48ee5e6f391d14e
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-07 09:35:14 +0100

    Create Navigation.jsx

---
+++ components/Navigation.jsx
@@ -0,0 +1,25 @@
+export default function Navigation() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href='#welcome'>Start</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#services'>Services</a>
+          </li>
+          <li>
+            <a href='#references'>References</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
