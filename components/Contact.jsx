7afe9d489a552f6aa258c8d6cf6922fecbd8b059
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-07 09:35:09 +0100

    Create Contact.jsx

---
+++ components/Contact.jsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <section id='contact'>
+      <h2>Contact</h2>
+      <p>Click the button to send me an e-mail and I will get back to you.</p>
+      <a href='mailto:stevefrenzel@gmx.de?subject=Enquiry about website or app'>
+        <button type='button'>Send E-Mail</button>
+      </a>
+    </section>
+  );
+}
