bb6f421ce5f3473095b288710ee4172b70396ac5
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-07 09:35:16 +0100

    Create References.jsx

---
+++ components/References.jsx
@@ -0,0 +1,12 @@
+export default function References() {
+  return (
+    <section id='references'>
+      <h2>References</h2>
+      <ul>
+        <li>Trve Peak Studio</li>
+        <li>AUF EWIG WINTER</li>
+        <li>Kora Winter</li>
+      </ul>
+    </section>
+  );
+}
