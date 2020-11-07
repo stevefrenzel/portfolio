5d0af4ac5cbbaf4b273deb2cf8352672a8fb168b
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-07 16:57:44 +0100

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -3,18 +3,22 @@ export default function About() {
     <section id='about'>
       <h2>About me</h2>
       <p>
-        I am not a web developer. At least I was not always. I also have over 10
-        years experience in the music business as a sound engineer, producer and
-        stagehand. Thanks to this time I learned a lot about people and how to
-        work with them. I have also learned to plan large and small projects of
-        all kinds, to execute them carefully and implement them successfully.
+        I am not a web developer. At least I was not always. I also have over{' '}
+        <mark>10 years experience</mark> in the music business as a sound
+        engineer, producer and stagehand.
       </p>
       <p>
-        Translating your vision to your customers (or visitors) is my main goal.
-        In order to achieve this, I constantly educate myself to be able to
-        decide which technology will best fit your project.The result will be an
-        intuitive, performant and easily accessible product, taking current
-        development best practices into account.
+        Thanks to this time I learned a lot about people and how to work with
+        them. I have also learned to <mark>plan large and small projects</mark>{' '}
+        of all kinds, to execute them carefully and implement them successfully.
+      </p>
+      <p>
+        <mark>Translating your vision</mark> to your customers (or visitors) is
+        my main goal. In order to achieve this, I constantly educate myself to
+        be able to decide which technology will best fit your project.The result
+        will be an{' '}
+        <mark>intuitive, performant and easily accessible product</mark>, taking
+        current development best practices into account.
       </p>
     </section>
   );
