bdd1ada7f948f19cd13f1533ec52e92104e3b786
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-08 09:44:23 +0100

    Create Welcome.jsx

---
+++ components/Welcome.jsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <section id='welcome'>
+      <h1>Steve Frenzel - Web Developer</h1>
+      <p>I build things for the web.</p>
+    </section>
+  );
+}
