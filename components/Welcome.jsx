f0d1ec67c86997d91eb654d135984454acc56704
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-08 13:11:57 +0100

    Update Welcome.jsx

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,8 +1,14 @@
 export default function Welcome() {
   return (
     <section id='welcome'>
-      <h1>Steve Frenzel - Web Developer</h1>
-      <p>I build things for the web.</p>
+      <img
+        src='/illustrations/feeling-proud.svg'
+        alt='Guy sitting on a table'
+      />
+      <span>
+        <h1>Steve Frenzel - Web Developer</h1>
+        <p>I build things for the web.</p>
+      </span>
     </section>
   );
 }
