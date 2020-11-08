ea51b236d28b085bb3a7fe0af67d719ea8803842
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-08 13:12:00 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 import '../styles/welcome.scss';
+import '../styles/about.scss';
+import '../styles/services.scss';
 import '../styles/contact.scss';
 import '../styles/footer.scss';
 
