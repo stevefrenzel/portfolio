1730a824f6975a97c50c883a022a27203f0a9084
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-13 17:21:29 +0100

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer id='footer'>
       <span>&copy; Steve Frenzel 2020</span>
-      <span>GitHub & LinkedIn</span>
+      {/* <span>GitHub & LinkedIn</span> */}
     </footer>
   );
 }
