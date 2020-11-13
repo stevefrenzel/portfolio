f66b237b71b7ab147c1cbe13bb1c200d83fa8e32
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-13 17:21:27 +0100

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,66 +1,71 @@
 export default function Contact() {
   return (
     <section id='contact'>
-      <h2>Contact</h2>
+      <div className='wrapper'>
+        <h2>Contact</h2>
+        <p>
+          <a href='mailto:stevefrenzel@gmx.de?subject=Enquiry about website or app'>
+            Click here to send an e-mail{' '}
+          </a>
+          or fill out the form below:
+        </p>
+        <form
+          name='contact'
+          method='post'
+          action='/success'
+          data-netlify='true'
+          netlify-honeypot='bot-field'
+        >
+          <fieldset>
+            {/* For Netlify: */}
+            <input type='hidden' name='form-name' value='contact' />
+            <label htmlFor='first-name'>First name:</label>
+            <input
+              className='contact-input'
+              type='text'
+              name='first-name'
+              id='first-name'
+              placeholder='First name'
+              required
+            ></input>
+            <label htmlFor='last-name'>Last name:</label>
+            <input
+              className='contact-input'
+              type='text'
+              name='last-name'
+              id='last-name'
+              placeholder='Last name'
+              required
+            ></input>
+            <label htmlFor='e-mail'>E-Mail:</label>
+            <input
+              className='contact-input'
+              type='email'
+              name='e-mail'
+              id='e-mail'
+              placeholder='E-Mail'
+              required
+            ></input>
+            <label htmlFor='enquiry'>Enquiry:</label>
+            <textarea
+              className='contact-input'
+              id='enquiry'
+              name='enquiry'
+              rows='5'
+              minLength='30'
+              placeholder='Enquiry'
+              required
+              spellCheck
+            ></textarea>
+            <button type='submit'>Submit</button>
+          </fieldset>
+        </form>
+      </div>
+
       <img
         src='/illustrations/inbox-cleanup.svg'
         alt='Ticked off letter inside envelope'
       />
-      <p>
-        <a href='mailto:stevefrenzel@gmx.de?subject=Enquiry about website or app'>
-          Click here to send me an e-mail{' '}
-        </a>
-        or fill out the form below:
-      </p>
-      <form
-        name='contact'
-        method='post'
-        action='/success'
-        data-netlify='true'
-        netlify-honeypot='bot-field'
-      >
-        {/* For Netlify: */}
-        <input type='hidden' name='form-name' value='contact' />
-        <label htmlFor='first-name'>First name:</label>
-        <input
-          className='contact-input'
-          type='text'
-          name='first-name'
-          id='first-name'
-          placeholder='First name'
-          required
-        ></input>
-        <label htmlFor='last-name'>Last name:</label>
-        <input
-          className='contact-input'
-          type='text'
-          name='last-name'
-          id='last-name'
-          placeholder='Last name'
-          required
-        ></input>
-        <label htmlFor='e-mail'>E-Mail:</label>
-        <input
-          className='contact-input'
-          type='email'
-          name='e-mail'
-          id='e-mail'
-          placeholder='Last name'
-          required
-        ></input>
-        <label htmlFor='enquiry'>Enquiry:</label>
-        <textarea
-          className='contact-input'
-          id='enquiry'
-          name='enquiry'
-          rows='5'
-          minLength='30'
-          placeholder='Enquiry'
-          required
-          spellCheck
-        ></textarea>
-        <button type='submit'>Send Enquiry</button>
-      </form>
     </section>
   );
 }
