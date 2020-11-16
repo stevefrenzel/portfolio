0c083a68fdd10b0c0f25e4bc3e44b7d529686683
Steve Frenzel <stevefrenzel@gmx.de>
2020-11-16 16:44:32 +0100

    Update Welcome.jsx

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,15 +1,23 @@
+import Image from 'next/image';
+
 export default function Welcome() {
   return (
     <section id='welcome'>
-      <div className='wrapper'>
-        <img
-          src='/illustrations/feeling-proud.svg'
-          alt='Guy sitting on a table'
-        />
-        <span>
-          <h1>Steve Frenzel - Web Developer</h1>
-          <p>I build things for the web.</p>
-        </span>
+      <div className='left-container perfect-center'>
+        <Image
+          src='/images/steve-frenzel.jpg'
+          width='400'
+          height='400'
+          alt='Steve Frenzel'
+        ></Image>
+      </div>
+
+      <div className='right-container perfect-center'>
+        <h1>
+          Steve Frenzel - <br />
+          Web Developer
+        </h1>
+        <p>I build things for the web.</p>
       </div>
     </section>
   );
