e5aa05a53e337d3df9244002756753bd72ebdbd6
Steve Frenzel <stevefrenzel@gmx.de>
2021-01-28 15:38:49 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,32 @@
-export const config = {
-  unstable_runtimeJS: false,
-};
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Header() {
+  const router = useRouter();
+
   return (
-    <div id='header-container'>
-      <h1>HEADER</h1>
-    </div>
+    <header>
+      <nav role='banner'>
+        <Link href='/'>
+          <a>🏚 Home</a>
+        </Link>
+        {router.locales.map((language) => (
+          <Link
+            href={router.asPath}
+            locale={language}
+            scroll={false}
+            key={language}
+          >
+            <a>
+              {language === 'en'
+                ? '🇺🇸 ENGLISH'
+                : language === 'de'
+                ? '🇩🇪 DEUTSCH'
+                : null}
+            </a>
+          </Link>
+        ))}
+      </nav>
+    </header>
   );
 }
