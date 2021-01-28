d32e397097a86204800590201b6ea68011474a51
Steve Frenzel <stevefrenzel@gmx.de>
2021-01-28 15:38:54 +0100

    Update Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,59 +1,56 @@
-// TODO Loop through data file to show projects
-// TODO Send respective props to selected component
-
-export const config = {
-  unstable_runtimeJS: false,
-};
-
+// Next.js
 import Link from 'next/link';
+import Image from 'next/image';
+// i18n
+import useTranslation from 'next-translate/useTranslation';
 
 export default function Work() {
+  const { t } = useTranslation('common');
+  const heading = t('headings.work');
+  const description = t('content.work');
+
   return (
-    <section id='work-container'>
+    <section id='work-wrapper'>
       <div className='heading-container'>
         <h2 id='work' tabIndex='-1'>
-          Work
+          {heading}
         </h2>
         <a href='#work' aria-label='Link to Work heading'>
           #
         </a>
       </div>
-      <p>
-        Here you can find a selection of projects I have worked on recently.
-        Click on the image to learn more!
-      </p>
+      <p>{description}</p>
 
       <div className='gallery'>
-        <Link href='/work/aufewigwinter'>
+        <Link href='/work/auf-ewig-winter'>
           <a>
-            <img
-              src='img/winter.jpg'
+            <Image
+              src='/img/winter.jpg'
               alt='AUF EWIG WINTER website'
               width='400'
               height='400'
-              loading='lazy'
             />
           </a>
         </Link>
         <Link href='/work/portfolio-website'>
           <a>
-            <img
-              src='img/portfolio.jpg'
+            <div className='image-wrapper'></div>
+            <Image
+              src='/img/portfolio.jpg'
               alt='Portfolio website'
               width='400'
               height='400'
-              loading='lazy'
             />
           </a>
         </Link>
         <Link href='/work/personal-blog'>
           <a>
-            <img
-              src='img/blog.jpg'
+            <div className='image-wrapper'></div>
+            <Image
+              src='/img/blog.jpg'
               alt='Personal blog'
               width='400'
               height='400'
-              loading='lazy'
             />
           </a>
         </Link>
