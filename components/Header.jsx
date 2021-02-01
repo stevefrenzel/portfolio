81abff1c2966cece5d2f7a57268cfbd15e0b28a3
Steve Frenzel <stevefrenzel@gmx.de>
2021-02-01 17:21:40 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,31 +1,44 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
 
 export default function Header() {
   const router = useRouter();
-
+  const { theme, setTheme } = useTheme();
+  const onSelectChange = (e) => {
+    const locale = e.target.value;
+    router.push(router.asPath, router.asPath, {
+      locale,
+      scroll: false,
+    });
+  };
   return (
     <header>
       <nav role='banner'>
         <Link href='/' scroll={false}>
-          <a>🏚 Home</a>
+          <a>✌️ SF</a>
         </Link>
-        {router.locales.map((language) => (
-          <Link
-            href={router.asPath}
-            locale={language}
-            scroll={false}
-            key={language}
-          >
-            <a>
-              {language === 'en'
-                ? '🇺🇸 ENGLISH'
-                : language === 'de'
-                ? '🇩🇪 DEUTSCH'
-                : null}
-            </a>
-          </Link>
-        ))}
+
+        <button
+          type='button'
+          onClick={
+            theme === 'dark'
+              ? () => setTheme('light')
+              : theme === 'light'
+              ? () => setTheme('dark')
+              : null
+          }
+        >
+          {theme === 'light' ? '🌞' : theme === 'dark' ? '🌚 ' : null}
+        </button>
+
+        <select name='languages' id='language-select' onChange={onSelectChange}>
+          {router.locales.map((language) => (
+            <option value={language} key={language}>
+              {language === 'en' ? '🇺🇸 EN' : language === 'de' ? '🇩🇪 DE' : null}
+            </option>
+          ))}
+        </select>
       </nav>
     </header>
   );
