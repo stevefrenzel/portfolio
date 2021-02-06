047e01f13f74733d1206b5b161781173913d3fe2
Steve Frenzel <stevefrenzel@gmx.de>
2021-02-06 08:48:08 +0100

    Create next-seo.config.js

---
+++ next-seo.config.js
@@ -0,0 +1,12 @@
+export default {
+  openGraph: {
+    type: 'website',
+    url: 'https://www.stevefrenzel.dev/',
+    description: 'Web developer focused on frontend, accessibility and SEO.',
+  },
+  // twitter: {
+  //   handle: '@stvfrnzl',
+  //   site: '@stvfrnzl',
+  //   cardType: 'summary_large_image',
+  // },
+};
