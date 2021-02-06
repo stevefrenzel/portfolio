be89972416cd631c64e33214c8e7d0692c3a185e
Steve Frenzel <stevefrenzel@gmx.de>
2021-02-06 08:48:17 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 // Next.js
-import Head from 'next/head';
+import { useRouter } from 'next/router';
+// next-seo
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 // Components
 import Header from '@components/Header';
 import Intro from '@components/Intro';
@@ -9,11 +12,16 @@ import Contact from '@components/Contact';
 import Footer from '@components/Footer';
 
 export default function Home() {
+  const router = useRouter();
   return (
     <>
-      <Head>
-        <title>Steve Frenzel - Web Developer</title>
-      </Head>
+      <DefaultSeo
+        {...SEO}
+        title='Steve Frenzel - Web Developer'
+        description='Web developer focused on frontend, accessibility and SEO.'
+        openGraph={{ locale: router.locale }}
+        canonical='https://www.stevefrenzel.dev/'
+      />
       <a href='#main'>Skip to main content</a>
       <Header />
       <Intro />
