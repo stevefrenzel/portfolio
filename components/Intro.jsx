451905bcdffcfe5666064e3ac76dabbb78b39c33
Steve Frenzel <stevefrenzel@gmx.de>
2021-02-07 11:09:12 +0100

    Update Intro.jsx

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -3,14 +3,11 @@ import Image from 'next/image';
 // i18n
 import useTranslation from 'next-translate/useTranslation';
 import Trans from 'next-translate/Trans';
-// next-themes
-// import { useTheme } from 'next-themes';
 
 export default function Intro() {
   const { t } = useTranslation('common');
   const jobTitle = t('headings.intro');
   const Component = (props) => <p {...props} />;
-  // const { theme } = useTheme();
   return (
     <section id='intro-wrapper'>
       <div className='image-and-text'>
@@ -22,23 +19,6 @@ export default function Intro() {
             height='400'
             priority
           />
-          {/* {theme === 'light' ? (
-            <Image
-              src='/img/steve-light.jpg'
-              alt='Picture of Steve Frenzel'
-              width='400'
-              height='400'
-              priority
-            />
-          ) : theme === 'dark' ? (
-            <Image
-              src='/img/steve-dark.jpg'
-              alt='Picture of Steve Frenzel'
-              width='400'
-              height='400'
-              priority
-            />
-          ) : null} */}
         </div>
         <div className='heading-wrapper'>
           <h1>Steve Frenzel</h1>
