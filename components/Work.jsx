1c9260f1f824f57be4c039645a660fba15a25525
Steve Frenzel <stevefrenzel@gmx.de>
2021-02-07 18:14:08 +0100

    Update Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,5 +1,6 @@
 // Next.js
 import Link from 'next/link';
+import Image from 'next/image';
 // i18n
 import useTranslation from 'next-translate/useTranslation';
 
@@ -7,18 +8,20 @@ export default function Work() {
   const { t } = useTranslation('common');
   const heading = t('headings.work');
   const description = t('content.work');
-
   return (
     <section id='work-wrapper'>
       <div className='heading-container'>
         <h2 id='work' tabIndex='-1'>
           {heading}
         </h2>
-        {/* <a href='#work' aria-label='Link to Work heading'>
-          #
-        </a> */}
+        <a href='#work' aria-label='Link to Work heading'>
+          <span role='img' aria-label='link symbol'>
+            🔗
+          </span>
+        </a>
       </div>
       <p>{description}</p>
+
       <ul>
         <li>
           <Link href='/work/auf-ewig-winter'>
