ac244e71fb7d088326de7b2e252e045a9522a4aa
Steve Frenzel <stevefrenzel@gmx.de>
2021-02-07 11:09:10 +0100

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,11 @@
+// TODO Add theme switch
+
+import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-// import { useTheme } from 'next-themes';
 
 export default function Header() {
   const router = useRouter();
-  // const { theme, setTheme } = useTheme();
   const onSelectChange = (e) => {
     const locale = e.target.value;
     router.push(router.asPath, router.asPath, {
@@ -17,29 +18,15 @@ export default function Header() {
     <header>
       <nav role='banner' id='navbar'>
         <Link href='/' scroll={false}>
-          <a>✌️ SF</a>
+          <a>✌️ Steve Frenzel</a>
         </Link>
 
         <div className='flex-item'></div>
 
-        {/* <button
-          aria-label='Button to switch themes'
-          type='button'
-          onClick={
-            theme === 'dark'
-              ? () => setTheme('light')
-              : theme === 'light'
-              ? () => setTheme('dark')
-              : null
-          }
-        >
-          {theme === 'light' ? '🌞' : theme === 'dark' ? '🌚 ' : null}
-        </button> */}
-
         <select name='languages' id='language-select' onChange={onSelectChange}>
           {router.locales.map((language) => (
             <option value={language} key={language}>
-              {language === 'en' ? 'EN' : language === 'de' ? 'DE' : null}
+              {language === 'en' ? 'EN 🇺🇸' : language === 'de' ? 'DE 🇩🇪' : null}
             </option>
           ))}
         </select>
